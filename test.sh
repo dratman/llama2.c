f5a239c7f5d1320e59d0192326599f9440a6c735
Ussama Zahid <ussamazahid96@gmail.com>
2024-03-11 02:08:14 +0300

    fix

--- test.sh
+++ test.sh
@@ -2,7 +2,7 @@
 
 python test.py \
     --checkpoint=outmini/ckpt.pt \
-    --tokenizer=data/tok4096.model \
+    --tokenizer=data/tok1024.model \
     --temperature=0.7 \
     --max_new_tokens=257
 
